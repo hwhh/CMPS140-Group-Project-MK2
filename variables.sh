7401218659cf6edae8fb87132f33d5a8e6b133a8
Henry Hargreaves <hwhh@st-andrews.ac.uk>
2018-05-08 19:23:12 -0700

    adding bash script for google ml engine

---
+++ variables.sh
@@ -0,0 +1,8 @@
+
+TRAIN_DIR=gs://$BUCKET_NAME/input/audio_train/
+TEST_DIR=gs://$BUCKET_NAME/input/audio_test/
+TEST_JSON=gs://$BUCKET_NAME/input/test.json
+TRAIN_DATA=gs://$BUCKET_NAME/input/train.csv
+EVAL_DATA=gs://$BUCKET_NAME/input/sample_submission.csv
+
+
