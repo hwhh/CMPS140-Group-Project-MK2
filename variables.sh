7c3e2bcb4538e2218f96d7563576da23b3449d0f
Henry Hargreaves <hwhh@st-andrews.ac.uk>
2018-05-17 18:24:39 -0700

    updated ignore

--- variables.sh
+++ variables.sh
@@ -1,5 +1,5 @@
-BUCKET_NAME=cmps140-model-1-mlengine
-OUTPUT_PATH=gs://cmps140-model-1-mlengine/mfcc_single_24/logs/
+BUCKET_NAME=cmps140-204319-mlengine
+OUTPUT_PATH=gs://$BUCKET_NAME/mfcc_single/logs/
 TRAIN_DIR=gs://$BUCKET_NAME/input/audio_train/
 TEST_DIR=gs://$BUCKET_NAME/input/audio_test/
 TEST_JSON=gs://$BUCKET_NAME/input/test.json
@@ -7,17 +7,17 @@ TRAIN_DATA=gs://$BUCKET_NAME/input/train.csv
 EVAL_DATA=gs://$BUCKET_NAME/input/sample_submission.csv
 REGION=us-central1
 
-gcloud ml-engine jobs submit training $JOB_NAME \
---job-dir $OUTPUT_PATH \
---runtime-version 1.4 \
---module-name trainer.task \
---package-path trainer/ \
---region $REGION \
---config=config.yaml \
--- \
---train-files $TRAIN_DATA \
---eval-files $EVAL_DATA \
---user_arg_1 $TRAIN_DIR \
---user_arg_2 $TEST_DIR \
---learning-rate 0.001 \
---verbosity DEBUG
+#gcloud ml-engine jobs submit training $JOB_NAME \
+#--job-dir $OUTPUT_PATH \
+#--runtime-version 1.4 \
+#--module-name trainer.task \
+#--package-path trainer/ \
+#--region $REGION \
+#--config=config.yaml \
+#-- \
+#--train-files $TRAIN_DATA \
+#--eval-files $EVAL_DATA \
+#--user_arg_1 $TRAIN_DIR \
+#--user_arg_2 $TEST_DIR \
+#--learning-rate 0.001 \
+#--verbosity DEBUG
